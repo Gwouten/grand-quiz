d9779c861be4ce70caa7ccfedc07231a7ef7c482
Gwouten <wouter.deloof@gmail.com>
2018-01-08 09:00:26 +0100

    05/08/2017 - evening

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
-import rootReducer from './modules';
+import quizFrReducer from './reducers/quizFrReducer';
 
 export const history = createHistory();
 
-const initialState = {}
 const enhancers = []
+const initialState = {}
 const middleware = [
   thunk,
   routerMiddleware(history)
@@ -27,7 +27,9 @@ const composedEnhancers = compose(
 )
 
 const store = createStore(
-  rootReducer,
+  combineReducers({
+    quizFr: quizFrReducer
+  }),
   initialState,
   composedEnhancers
 )
