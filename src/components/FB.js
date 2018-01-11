c2c38b12159d6e47ed775059311b21b0a91fdc14
Gwouten <wouter.deloof@gmail.com>
2018-01-11 17:57:27 +0100

    attempt for FB sharing via Reddit solution, part 2

--- src/components/FB.js
+++ src/components/FB.js
@@ -5,13 +5,14 @@
  */
 export const shareResultToFacebook = (result, lang) => {
     const params = {
+        href: encodeURIComponent(`${lang ? 'http://concours.concerto.space/' : 'http://quizz.concerto.space/' }`),
         u: result,
         picture: `${lang ? 'http://concours.concerto.space/img/GrandQuizz2018_logo_fr.svg' : 'http://quizz.concerto.space/img/GrandQuizz2018_logo_nl.svg'}`,
-        title: `${lang ? 'Grand Quiz 2018' : 'De Grote QuizZ 2018'}`,
-        description: `${lang ? 'Mon résultat du Grand Quiz 2018' : 'Mijn resultaat van De Grote QuizZ 2018'}`,
+        title: `${lang ? 'Grand Quiz 2018' : 'De Grote QuizZ 2018'}`.replace(/\s/gi,'-'),
+        description: `${lang ? 'Mon résultat du Grand Quiz 2018' : 'Mijn resultaat van De Grote QuizZ 2018'}`.replace(/\s/gi,'-'),
     };
-
-    const url = `https://www.facebook.com/sharer/sharer.php?${serialize(params)}`;
+    console.log(params);
+    const url = `https://www.facebook.com/dialog/share?app_id=1625426427527020&display=popup&${serialize(params)}`;
 
     return url;
 };
@@ -25,3 +26,5 @@ export const shareResultToFacebook = (result, lang) => {
 const serialize = params => Object.keys(params)
 .map(param => `${param}=${encodeURIComponent(params[param])}`)
 .join('&');
+
+// https://www.facebook.com/dialog/share?app_id=5303202981&display=popup&href=http%3A%2F%2Fwww.divercitymag.be%2Fnl%2Fde-grote-verkeersaders-alternatief-voor-de-teloorgang-van-de-winkelcentra%2F&redirect_uri=https%3a%2f%2fstatic.addtoany.com%2fmenu%2fthanks.html%23url%3dhttp%3A%2F%2Fwww.divercitymag.be%2Fnl%2Fde-grote-verkeersaders-alternatief-voor-de-teloorgang-van-de-winkelcentra%2F&quote=
