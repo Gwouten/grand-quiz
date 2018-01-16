2a1ea572c4233dcfc7f84ceb23e193b40b1254f9
Gwouten <wouter.deloof@gmail.com>
2018-01-16 15:55:15 +0100

    fixed bug that selects corresponding logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component{
     render(){
         return (
             <div className="logo__wrapper">
-                <img className="quiz-logo" src={this.props.lang ? 'img/GrandQuizz2018_logo_fr.svg' : 'img/GrandQuizz2018_logo_nl.svg'} alt="Grand Quiz 2018" />
+                <img className="quiz-logo" src={this.props.lang === 'fr' ? 'img/GrandQuizz2018_logo_fr.svg' : 'img/GrandQuizz2018_logo_nl.svg'} alt="Grand Quiz 2018" />
                 <a href="http://www.concerto.be/"><img className="concerto-logo" src="img/concerto.svg" alt="Grand Quiz 2018" /></a>
             </div>
         );
